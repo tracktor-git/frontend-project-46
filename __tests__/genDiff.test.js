16a9f07064ff48daa9e676a51fae65b510bf6bc7
Tracktor <tracktor@bk.ru>
2023-01-20 12:13:27 +0300

    Modify tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -5,20 +5,16 @@ import { getFixturePath, genDiff } from '../index.js';
 const diff1 = getFixturePath('diff1.txt');
 const expected = readFileSync(diff1, 'utf-8');
 
-test('Geneate flat diff JSON', () => {
+test('Geneate flat diffs', () => {
   const path1 = getFixturePath('file1.json');
   const path2 = getFixturePath('file2.json');
-  const result = genDiff(parse(path1), parse(path2));
+  const path3 = getFixturePath('flat1.yaml');
+  const path4 = getFixturePath('flat2.yml');
+  const result1 = genDiff(parse(path1), parse(path2));
+  const result2 = genDiff(parse(path3), parse(path4));
 
-  expect(result).toBe(expected);
-  expect(typeof result).toBe('string');
-});
-
-test('generate flat diff YAML', () => {
-  const path1 = getFixturePath('flat1.yaml');
-  const path2 = getFixturePath('flat2.yml');
-  const result = genDiff(parse(path1), parse(path2));
-
-  expect(result).toBe(expected);
-  expect(typeof result).toBe('string');
+  expect(result1).toBe(expected);
+  expect(result2).toBe(expected);
+  expect(typeof result1).toBe('string');
+  expect(typeof result2).toBe('string');
 });
