c9ddeac558fc94941f775759bb0933482a4a7ae3
Tracktor <tracktor@bk.ru>
2023-01-31 16:13:14 +0300

    Modify plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -13,19 +13,19 @@ const generateString = (item) => {
 const plain = (data, parent = '') => data.map((node) => {
   const { status, key } = node;
   const path = [parent, key].filter((item) => item).join('.');
+  const {
+    previous, current, value, children,
+  } = node;
   if (status === 'removed') {
     return `Property '${path}' was removed`;
   }
   if (status === 'updated') {
-    const { previous, current } = node;
     return `Property '${path}' was updated. From ${generateString(previous)} to ${generateString(current)}`;
   }
   if (status === 'added') {
-    const { value } = node;
     const added = generateString(value);
     return `Property '${path}' was added with value: ${added}`;
   }
-  const { children } = node;
   return status === 'nested' ? plain(children, path) : null;
 }).filter((item) => item).join('\n');
 
